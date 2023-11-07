8f10dd0f9e3c0a6a09fa4f1d8659f04aad25f887
Hess Lewis <hess@ali-lewis.com>
2023-11-07 16:58:17 -0500

    update framework

--- build.py
+++ build.py
@@ -1,11 +1,9 @@
-PROJECT_DIR = "."
-COMMON_TEMPLATE_DIR = f'{PROJECT_DIR}/template_common'
-CLIENT_TEMPLATE_DIR = f'{PROJECT_DIR}/template_client'
-SERVER_TEMPLATE_DIR = f'{PROJECT_DIR}/template_server'
-BUILD_DIR = f'{PROJECT_DIR}/build'
-MMC_BUILD = f'{BUILD_DIR}/mmc'
+# Hess Lewis
+# 07/11/2023
+# I am not a python programer (:
 
 import os
+import subprocess
 from os import path
 import shutil
 from zipfile import ZipFile
@@ -34,21 +32,42 @@ def merge_directories(new_directory, source_dir1, source_dir2):
             else:
                 shutil.copy2(source_item, destination_item)
 
-# start
+# DECLARE
+git_process = subprocess.run(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE)
+COMMIT_ID = str(git_process.stdout)
+COMMIT_ID = COMMIT_ID[2:-3]
+PROJECT_DIR = "."
+COMMON_TEMPLATE_DIR = f'{PROJECT_DIR}/template_common'
+CLIENT_TEMPLATE_DIR = f'{PROJECT_DIR}/template_client'
+SERVER_TEMPLATE_DIR = f'{PROJECT_DIR}/template_server'
+MMC_TEMPLATE_DIR = f'{PROJECT_DIR}/template_mmc'
+BUILD_DIR = f'{PROJECT_DIR}/build/{COMMIT_ID}'
+MMC_BUILD_DIR = f'{BUILD_DIR}/MultiMC'
+CLIENT_BUILD_DIR = f'{BUILD_DIR}/dot_minecraft'
+SERVER_BUILD_DIR = f'{BUILD_DIR}/server'
+
+# START
+print(f'Build version \'{COMMIT_ID}\'')
+
+if not path.exists(f'{PROJECT_DIR}/build'):
+    os.mkdir(f'{PROJECT_DIR}/build')
+
 if path.exists(BUILD_DIR):
-    print(f'Please delete {BUILD_DIR}')
+    print('Build already completed')
     exit(0)
 
 os.mkdir(BUILD_DIR)
-# generate .minecraft
-client = f'{BUILD_DIR}/client'
-merge_directories(client, COMMON_TEMPLATE_DIR, CLIENT_TEMPLATE_DIR)
-server = f'{BUILD_DIR}/server'
-merge_directories(server, COMMON_TEMPLATE_DIR, SERVER_TEMPLATE_DIR)
 
-mmc = MMC_BUILD
-os.mkdir(MMC_BUILD)
-shutil.copy(f'{PROJECT_DIR}/mmc-pack.json', MMC_BUILD)
-shutil.copy(f'{PROJECT_DIR}/instance.cfg', MMC_BUILD)
-merge_directories(f'{MMC_BUILD}/.minecraft', COMMON_TEMPLATE_DIR, CLIENT_TEMPLATE_DIR)
-shutil.make_archive(f'{BUILD_DIR}/Purposeful_Create', 'zip', MMC_BUILD)
+# generate .minecraft
+print("Generating '.minecraft'")
+merge_directories(CLIENT_BUILD_DIR, COMMON_TEMPLATE_DIR, CLIENT_TEMPLATE_DIR)
+merge_directories(SERVER_BUILD_DIR, COMMON_TEMPLATE_DIR, SERVER_TEMPLATE_DIR)
+
+# build multimc
+print("Generating MultiMC directory")
+shutil.copytree(MMC_TEMPLATE_DIR, MMC_BUILD_DIR)
+shutil.copytree(CLIENT_BUILD_DIR, f'{MMC_BUILD_DIR}/.minecraft')
+
+print("Zipping MultiMC")
+shutil.make_archive(f'{PROJECT_DIR}/build/Purposeful_Create-MultiMC-{COMMIT_ID}', 'zip', MMC_BUILD_DIR)
+
